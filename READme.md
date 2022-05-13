cd8ffcb2090b1a54ab8191082e7ff8529a3cd8a7
habfem <habeebmustapha96@gmail.com>
2022-05-13 03:27:15 -0500

    React Boiler Plate

---
+++ READme.md
@@ -0,0 +1,5 @@
+# E-commerce App with MERN Stack
+
+## React Frontend
+
+```npx create-react-app@latest frontend --use-npm```
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <>
+     <h1>Welcome to HaShop</h1> 
+    </>
+  );
+}
+
+export default App;
