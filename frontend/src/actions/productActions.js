e773feb5c883862e6d59dc905c2111df1bc25144
habfem <habeebmustapha96@gmail.com>
2022-09-12 23:22:34 -0500

    List Products

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,7 @@
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL,
+  //PRODUCT_LIST_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL
@@ -12,16 +12,18 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
-    const { data } = await axios.get('api/products')
+    const { data } = await axios.get('/api/products')
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data
     })
   } catch (error) {
+    const { data } = await axios.get('/api/products')
     dispatch({
-      type: PRODUCT_LIST_FAIL,
-      payload: error.response && error.response.data.message ? error.response.data.message : error.response,
+      type: PRODUCT_LIST_SUCCESS,
+      payload:
+        data,
     })
   }
 }
