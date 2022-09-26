d87c6aebf971ed9c078f6849775cf585498fbbae
habfem <habeebmustapha96@gmail.com>
2022-09-25 19:11:44 -0500

    Search Bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav, NavDropdown, } from 'react-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
@@ -22,6 +23,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <SearchBox />
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 import Product from '../Product'
 import Message from '../Message'
 import Loader from '../Loader'
 import { listProducts } from '../../actions/productActions'
 const HomeScreen = () => {
+  const { keyword } = useParams()
+
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
   const { loading, error, products } = productList
 
   useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
